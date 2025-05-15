c1fcd66e0c3d8263efc57f84f0547516ac1a3279
mira <mkyshchak@gmail.com>
2025-05-15 19:36:49 +0300

    state

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "tfstate"
-    storage_account_name  = "kukuruki4"
-    container_name        = "tfstate"
-    key                   = "prod.tfstate"
-    use_oidc              = true
+    resource_group_name  = "tfstate"
+    storage_account_name = "kukuruki4"
+    container_name       = "tfstate"
+    key                  = "prod.tfstate"
+    use_oidc             = true
   }
 }
 
